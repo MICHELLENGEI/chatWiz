b584828950fd34778c076d746852e7b7493fe3f9
MICHELLLENGEI <michellengei20@gmail.com>
2023-05-24 16:45:00 +0300

    commit

--- app/src/main/java/com/example/chatwizapplication/SignupActivity.kt
+++ app/src/main/java/com/example/chatwizapplication/SignupActivity.kt
@@ -1,11 +1,10 @@
 package com.example.chatwizapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.util.Patterns
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.chatwizapplication.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -30,8 +29,10 @@ class SignupActivity : AppCompatActivity() {
         binding.btnSignup2.setOnClickListener {
             val name = binding.etNameSignUp.text.toString()
             val email = binding.etEmailSignUp.text.toString()
+            val phone = binding.etPhoneNumber.text.toString()
             val password = binding.etPasswordSignUp.text.toString()
             val confirmPassword = binding.etConfirmPassword.text.toString()
+
 
             if (checkFields()) {
                 auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
@@ -49,9 +50,10 @@ class SignupActivity : AppCompatActivity() {
                         val hashMap: HashMap<String, String> = HashMap()
                         hashMap.put("userId", userId)
                         hashMap.put("username", name)
+                        hashMap.put("phoneNumber", phone)
                         hashMap.put("confirmPassword", confirmPassword)
 
-                        databaseReference.setValue(hashMap).addOnCompleteListener() {  dbTask->
+                        databaseReference.setValue(hashMap).addOnCompleteListener() { dbTask ->
 
                             if (dbTask.isSuccessful) {
                                 val intent3 = Intent(this@SignupActivity, HomeActivity::class.java)
@@ -72,14 +74,30 @@ class SignupActivity : AppCompatActivity() {
     }
 
     private fun checkFields(): Boolean {
+        val name = binding.etNameSignUp.text.toString()
         val emailS = binding.etEmailSignUp.text.toString()
+        val phone = binding.etPhoneNumber.text.toString()
         val passwordS = binding.etPasswordSignUp.text.toString()
         val confirmPass = binding.etConfirmPassword.text.toString()
-        if (emailS == "") {
+        val regex = Regex("^(07|01)\\d{8}$")
+        val pattern = Regex("^\\w+@gmail\\.com$")
+
+
+
+        if (name == "") {
+            Toast.makeText(this, "Name is a required field", Toast.LENGTH_SHORT).show()
+            return false
+        } else if (emailS == "") {
             Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show()
             return false
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailS).matches()) {
+        } else if (!pattern.matches(emailS)) {
             Toast.makeText(this, "Check the Email format", Toast.LENGTH_SHORT).show()
+            return false
+        } else if (phone == "") {
+            Toast.makeText(this, "Phone Number is required", Toast.LENGTH_SHORT).show()
+            return false
+        } else if (!regex.matches(phone)) {
+            Toast.makeText(this, "Invalid phone number", Toast.LENGTH_SHORT).show()
             return false
         } else if (passwordS == "") {
             Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show()
